d78d7cace37b30df9465abc1ae357eeaa08922bf
quanghuy <quanghuydang1902@gmail.com>
2019-08-02 18:21:14 +0700

    update data

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import gensim
 import numpy as np
+from collections import Counter
 import os
 from distutils.version import LooseVersion
 
@@ -28,5 +29,52 @@ def load_embedding_file(embed_file_name, word_set):
 
     else:
         print(
-            "Download word2vec model and put into ./data/. File: https://drive.google.com/open?id=0B1GKSX6YCHXlakkzQ2plZVdUUE0")
+            "Download word2vec model and put into ./data/. File: "
+            "https://drive.google.com/open?id=0B1GKSX6YCHXlakkzQ2plZVdUUE0")
     return embeddings
+
+
+def get_dataset_resources(data_file_name, sent_word2idx, target_word2idx, word_set, max_sent_len):
+
+    if len(sent_word2idx) == 0:
+        sent_word2idx["<pad>"] = 0
+
+    word_count = []
+    sent_word_count = []
+    target_count = []
+
+    words = []
+    sentence_words = []
+    target_words = []
+
+    with open(data_file_name, 'r') as data_file:
+        lines = data_file.read().split('\n')
+        for line_no in range(0, len(lines) - 1, 3):
+            sentence = lines[line_no]
+            target = lines[line_no + 1]
+
+            sentence = sentence.replace("$T$", "")
+            sentence = sentence.lower()
+            target = target.lower()
+            max_sent_len = max(max_sent_len, len(sentence.split()))
+            sentence_words.extend(sentence.split())
+            target_words.extend([target])
+            words.extend(sentence.split() + target.split())
+
+        sent_word_count.extend(Counter(sentence_words).most_common())
+        target_count.extend(Counter(target_words).most_common())
+        word_count.extend(Counter(words).most_common())
+
+        for word, _ in sent_word_count:
+            if word not in sent_word2idx:
+                sent_word2idx[word] = len(sent_word2idx)
+
+        for target, _ in target_count:
+            if target not in target_word2idx:
+                target_word2idx[target] = len(target_word2idx)
+
+        for word, _ in word_count:
+            if word not in word_set:
+                word_set[word] = 1
+
+    return max_sent_len
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 import pprint
 import tensorflow as tf
 
-from nltk import word_tokenize
 from data import *
 
 pp = pprint.PrettyPrinter()
@@ -18,10 +17,10 @@ flags.DEFINE_float("init_lr", 0.01, "initial learning rate [0.01]")
 flags.DEFINE_float("init_hid", 0.1, "initial internal state value [0.1]")
 flags.DEFINE_float("init_std", 0.01, "weight initialization std [0.05]")
 flags.DEFINE_float("max_grad_norm", 100, "clip gradients to this norm [50]")
-flags.DEFINE_string("pretrain_file", "/data/wiki.vi.model.bin",
+flags.DEFINE_string("pretrain_file", "/data/iphone_train.txt",
                     "pre-trained glove vectors file path [../data/wiki.vi.model.bin]")
-flags.DEFINE_string("train_data", "data/Laptops_Train.xml.seg",
-                    "train gold data set path [./data/Laptops_Train.xml.seg]")
+flags.DEFINE_string("train_data", "data/iphone_train.txt",
+                    "train gold data set path [./data/iphone_train.txt]")
 flags.DEFINE_string("test_data", "data/Laptops_Test_Gold.xml.seg",
                     "test gold data set path [./data/Laptops_Test_Gold.xml.seg]")
 flags.DEFINE_boolean("show", False, "print progress [False]")
@@ -31,6 +30,55 @@ flags.DEFINE_integer("mem_size", 83, "mem_size")
 
 FLAGS = flags.FLAGS
 
+
+def main(_):
+    source_count, target_count = [], []
+    source_word2idx, target_word2idx, word_set = {}, {}, {}
+    max_sent_len = -1
+
+    max_sent_len = get_dataset_resources(FLAGS.train_data, source_word2idx, target_word2idx, word_set, max_sent_len)
+    max_sent_len = get_dataset_resources(FLAGS.test_data, source_word2idx, target_word2idx, word_set, max_sent_len)
+    embeddings = load_embedding_file(FLAGS.pretrain_file, word_set)
+
+
+
+    # train_data = get_dataset(FLAGS.train_data, source_word2idx, target_word2idx, embeddings)
+    # test_data = get_dataset(FLAGS.test_data, source_word2idx, target_word2idx, embeddings)
+    #
+    # print("train data size - ", len(train_data[0]))
+    # print("test data size - ", len(test_data[0]))
+    #
+    # print("max sentence length - ", max_sent_len)
+    #
+    # FLAGS.pad_idx = source_word2idx['<pad>']
+    # FLAGS.nwords = len(source_word2idx)
+    # FLAGS.mem_size = max_sent_len
+    #
+    # pp.pprint(flags.FLAGS.__flags)
+    #
+    # print('loading pre-trained word vectors...')
+    # print('loading pre-trained word vectors for train and test data')
+    #
+    # pre_trained_context_wt, pre_trained_target_wt = get_embedding_matrix(embeddings, source_word2idx,
+    #                                                                      target_word2idx, FLAGS.edim)
+    #
+    # with tf.Session() as sess:
+    #     model = MemN2N(FLAGS, sess, pre_trained_context_wt, pre_trained_target_wt)
+    #     model.build_model()
+    #     model.run(train_data, test_data)
+
+
+# if __name__ == '__main__':
+#     tf.app.run()
+
+
 if __name__ == '__main__':
-    embeddings = load_embedding_file(FLAGS.pretrain_file, [])
-    print(embeddings)
+    # embeddings = load_embedding_file(FLAGS.pretrain_file, [])
+    # print(embeddings)
+    source_count, target_count = [], []
+    source_word2idx, target_word2idx, word_set = {}, {}, {}
+    max_sent_len = -1
+
+    max_sent_len = get_dataset_resources(FLAGS.train_data, source_word2idx, target_word2idx, word_set, max_sent_len)
+    max_sent_len = get_dataset_resources(FLAGS.test_data, source_word2idx, target_word2idx, word_set, max_sent_len)
+    embeddings = load_embedding_file(FLAGS.pretrain_file, word_set)
