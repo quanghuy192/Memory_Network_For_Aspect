bebf665ab7de5672f8acb1c7bbc3e4e291479a17
quanghuy <quanghuydang1902@gmail.com>
2020-03-17 15:23:38 +0700

    update data

--- data/data_crawl.py
+++ data/data_crawl.py
@@ -15,71 +15,49 @@ while i > 0 and status:
     # print(url)
     page = requests.get(url)
 
-    if page.status_code != 200:
-        # print("false")
-        status = False
-    else:
-        soup = BeautifulSoup(page.content, 'html.parser')
+        if page.status_code != 200:
+            # print("false")
+            status = False
+        else:
+            soup = BeautifulSoup(page.content, 'html.parser')
 
-        # start crawl quote
-        quote_list = soup.find_all('div', class_='quote')
-        for item in list(set(quote_list)):
-            data_export.writelines(item.get_text() + '\n\n')
-            # print("")
+            # start crawl quote
+            quote_list = soup.find_all('div', class_='quote')
+            for item in list(set(quote_list)):
+                data_export.writelines(item.get_text() + '\n\n')
+                # print("")
 
-        soup_content = BeautifulSoup(page.content, 'html.parser')
+            soup_content = BeautifulSoup(page.content, 'html.parser')
 
-        script_list = soup_content.find_all('script')
-        for item in list(set(script_list)):
-            content = item.get_text()
-            if '__NEXT_DATA__' in content:
-                json_content = content.split(' = ')[1].split(';__')[0]
-                data = json.loads(json_content)
-                pageProps = data['props']['pageProps']
-                api_data_comment = pageProps['apiData']
+            script_list = soup_content.find_all('script')
+            for item in list(set(script_list)):
+                content = item.get_text()
+                if '__NEXT_DATA__' in content:
+                    json_content = content.split(' = ')[1].split(';__')[0]
+                    try:
+                        data = json.loads(json_content)
+                    except json.decoder.JSONDecodeError:
+                        print(json_content)
+                        break
 
-                for key in api_data_comment:
-                    posts_dict = api_data_comment[key]
-                    if 'posts' in posts_dict:
-                        for key_post in posts_dict:
-                            if type(posts_dict[key_post]) == list:
-                                thread_post = posts_dict[key_post]
-                                for comment in thread_post:
-                                    comment_content = comment['post_body']
-                                    if '[CENTER]' not in comment_content:
-                                        if '[/QUOTE]' in comment_content:
-                                            data_export.writelines(comment_content.split('[/QUOTE]')[1] + '\n')
-                                        else:
-                                            data_export.writelines(comment_content + '\n\n')
+                    pageProps = data['props']['pageProps']
+                    api_data_comment = pageProps['apiData']
 
-        print('========================= Crawling ==================================')
-    i = i + 1
+                    for key in api_data_comment:
+                        posts_dict = api_data_comment[key]
+                        if 'posts' in posts_dict:
+                            for key_post in posts_dict:
+                                if type(posts_dict[key_post]) == list:
+                                    thread_post = posts_dict[key_post]
+                                    for comment in thread_post:
+                                        comment_content = comment['post_body']
+                                        if '[CENTER]' not in comment_content:
+                                            if '[/QUOTE]' in comment_content:
+                                                data_export.writelines(comment_content.split('[/QUOTE]')[1] + '\n')
+                                            else:
+                                                data_export.writelines(comment_content + '\n\n')
 
-
-# url = "https://tinhte.vn/thread/cam-nhan-iphone-11-pro-max-sau-3-tuan-chang-khac-gi-iphone-xs-max.3024584/"
-# page = requests.get(url)
-# soup = BeautifulSoup(page.content, 'html.parser')
-#
-# script_list = soup.find_all('script')
-# for item in list(set(script_list)):
-#     content = item.get_text()
-#     if '__NEXT_DATA__' in content:
-#         json_content = content.split(' = ')[1].split(';__')[0]
-#         data = json.loads(json_content)
-#         api_data_comment = data["props"]["pageProps"]["apiData"]
-#
-#         for key in api_data_comment:
-#             posts_dict = api_data_comment[key]
-#             if 'posts' in posts_dict:
-#                 for key_post in posts_dict:
-#                     if type(posts_dict[key_post]) == list:
-#                         thread_post = posts_dict[key_post]
-#                         for comment in thread_post:
-#                             comment_content = comment['post_body']
-#                             if '[CENTER]' not in comment_content:
-#                                 if '[/QUOTE]' in comment_content:
-#                                     data_export.writelines(comment_content.split('[/QUOTE]')[1] + '\n')
-#                                 else:
-#                                     data_export.writelines(comment_content + '\n\n')
+        i = i + 1
 data_export.close()
+data_source.close()
 
