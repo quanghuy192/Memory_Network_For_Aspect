5e345d7c928014391c5b503243cd31f9a26791ee
quanghuy <quanghuydang1902@gmail.com>
2020-05-29 18:19:27 +0700

    labeled data

--- model.py
+++ model.py
@@ -251,31 +251,31 @@ class MemN2N(object):
                 self.mask: mask})
 
             sentence_list = []
-            # if is_train:
-            #     with open('iphone_train.txt', 'r') as data_file:
-            #         lines = data_file.read().split('\n')
-            #         for line_no in range(0, len(lines) - 1, 3):
-            #             sentence = lines[line_no].lower()
-            #             sentence_list.append(sentence)
-            if not is_train:
-                with open('iphone_test.txt', 'r') as data_file:
+            if is_train:
+                with open('iphone_train.txt', 'r') as data_file:
                     lines = data_file.read().split('\n')
                     for line_no in range(0, len(lines) - 1, 3):
                         sentence = lines[line_no].lower()
                         sentence_list.append(sentence)
+            # if not is_train:
+            #     with open('iphone_test.txt', 'r') as data_file:
+            #         lines = data_file.read().split('\n')
+            #         for line_no in range(0, len(lines) - 1, 3):
+            #             sentence = lines[line_no].lower()
+            #             sentence_list.append(sentence)
 
-            # if is_train:
-            #     for b in xrange(self.batch_size):
-            #         if raw_labels[b] != predictions[b]:
-            #             print(" predict raw_labels : " + str(raw_labels[b]))
-            #             print(" predict by system : " + str(predictions[b]))
-            #             print(sentence_list[i] + " \n")
-            if not is_train:
+            if is_train:
                 for b in xrange(self.batch_size):
                     if raw_labels[b] != predictions[b]:
                         print(" predict raw_labels : " + str(raw_labels[b]))
                         print(" predict by system : " + str(predictions[b]))
                         print(sentence_list[i] + " \n")
+            # if not is_train:
+            #     for b in xrange(self.batch_size):
+            #         if raw_labels[b] != predictions[b]:
+            #             print(" predict raw_labels : " + str(raw_labels[b]))
+            #             print(" predict by system : " + str(predictions[b]))
+            #             print(sentence_list[i] + " \n")
 
             for b in xrange(self.batch_size):
                 if b >= len(raw_labels): break
--- test.py
+++ test.py
@@ -3,42 +3,43 @@
 from underthesea import word_tokenize
 from underthesea import pos_tag
 import csv
-try:
-    import cPickle as pickle
-except:
-    import pickle
-
-# vn_sentences = word_tokenize('Do dịch bệnh, nên sinh viên được nghỉ thêm một tuần', format='text')
-# vn_sentences_pos_tag = pos_tag(vn_sentences)
-# [('Do', 'E'), ('dịch_bệnh', 'N'), (',', 'CH'), ('nên', 'C'), ('sinh_viên', 'V'), ('được', 'R'), ('nghỉ', 'V'), ('thêm', 'V'), ('một', 'M'), ('tuần', 'N')]
+# try:
+#     import cPickle as pickle
+# except:
+#     import pickle
 #
-# print(vn_sentences)
-# print(vn_sentences_pos_tag)
-
-with open('foody_data.pkl', 'rb') as fp:
-    data_frame_foody_data = pickle.load(fp)
-    print(type(data_frame_foody_data))
-    # print(data_frame_foody_data)
-    # for col in data_frame_foody_data.columns:
-    #     print(col)
-
-    data_frame_foody_data_filtered = data_frame_foody_data[['review_content', 'avg_score', 'location_point', 'space_point', 'quality_point',
-                                                            'service_point', 'price_point']]
-
-    data_frame_foody_data_filtered1 = data_frame_foody_data[
-        ['review_content']]
-
-    data_frame_foody_data_filtered2 = data_frame_foody_data[
-        ['location_point', 'space_point', 'quality_point',
-         'service_point', 'price_point']]
-
-    print(data_frame_foody_data_filtered1)
-
-    print(data_frame_foody_data_filtered2)
-
-    f = open("data.txt", "w+")
-    for index, row in data_frame_foody_data_filtered1.iterrows():
-        print(row['review_content'])
-        f.writelines(row['review_content'])
-    f.close()
+# # vn_sentences = word_tokenize('Do dịch bệnh, nên sinh viên được nghỉ thêm một tuần', format='text')
+# # vn_sentences_pos_tag = pos_tag(vn_sentences)
+# # [('Do', 'E'), ('dịch_bệnh', 'N'), (',', 'CH'), ('nên', 'C'), ('sinh_viên', 'V'), ('được', 'R'), ('nghỉ', 'V'), ('thêm', 'V'), ('một', 'M'), ('tuần', 'N')]
+# #
+# # print(vn_sentences)
+# # print(vn_sentences_pos_tag)
+#
+# with open('foody_data.pkl', 'rb') as fp:
+#     data_frame_foody_data = pickle.load(fp)
+#     print(type(data_frame_foody_data))
+#     # print(data_frame_foody_data)
+#     # for col in data_frame_foody_data.columns:
+#     #     print(col)
+#
+#     data_frame_foody_data_filtered = data_frame_foody_data[['review_content', 'avg_score', 'location_point', 'space_point', 'quality_point',
+#                                                             'service_point', 'price_point']]
+#
+#     data_frame_foody_data_filtered1 = data_frame_foody_data[
+#         ['review_content']]
+#
+#     data_frame_foody_data_filtered2 = data_frame_foody_data[
+#         ['location_point', 'space_point', 'quality_point',
+#          'service_point', 'price_point']]
+#
+#     print(data_frame_foody_data_filtered1)
+#
+#     print(data_frame_foody_data_filtered2)
+#
+#     f = open("data.txt", "w+")
+#     for index, row in data_frame_foody_data_filtered1.iterrows():
+#         print(row['review_content'])
+#         f.writelines(row['review_content'])
+#     f.close()
+import timeit
 
